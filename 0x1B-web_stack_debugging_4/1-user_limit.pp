649781f550c570f6d5cae31edf2fc4b6e33e91d6
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-11-10 11:28:09 +0000

    debug

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,10 @@
 # Change the OS configuration so that it is possible to login with the user
-exec { 'fix-toomanyfiles':
-	command => "sed -i 's/holberton hard nofile 5/holberton hard nofile 1605972/g' /etc/security/limits.conf; sed -i 's/holberton soft nofile 4/holberton soft nofile 1048576/g' /etc/security/limits.conf",
-	path    => ['/bin', '/usr/bin', '/usr/sbin']
+exec { 'hard-file-limit-increased':
+	command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+	path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'soft-file-limit-increased':
+	command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+	path    => '/usr/local/bin/:/bin/'
 }
