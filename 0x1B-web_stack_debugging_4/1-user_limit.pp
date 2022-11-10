06559570aafb5bb0cef3b807f4b8ea28a5ea8855
root <100032916+ImzyO@users.noreply.github.com>
2022-11-10 04:20:49 -0800

    debug whoitespaces

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,19 +1,5 @@
-<<<<<<< HEAD
 # User limit
-exec { 'increase-hard-file-limit-for-holberton-user':
-=======
-# Change the OS configuration so that it is possible to login with the user
-exec { 'hard-file-limit-increased':
->>>>>>> 97ef494af1a54536d1964eeeb28972d58e33b4aa
-	command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
-	path    => '/usr/local/bin/:/bin/'
-}
-
-<<<<<<< HEAD
-exec { 'increase-soft-file-limit-for-holberton-user':
-=======
-exec { 'soft-file-limit-increased':
->>>>>>> 97ef494af1a54536d1964eeeb28972d58e33b4aa
-	command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
-	path    => '/usr/local/bin/:/bin/'
+exec { 'user-limit':
+  command => "sed -i 's/holberton hard nofile 5/holberton hard nofile 1605972/g' /etc/security/limits.conf; sed -i 's/holberton soft nofile 4/holberton soft nofile 1048576/g' /etc/security/limits.conf",
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
 }
