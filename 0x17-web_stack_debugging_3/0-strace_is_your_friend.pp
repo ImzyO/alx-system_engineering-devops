c42801637a9999f92d557418fe284a1f27ea51b3
root <100032916+ImzyO@users.noreply.github.com>
2022-11-10 04:14:37 -0800

    debug

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # fix it and then automate it using Puppet
 exec { 'fix_phpp':
-	provider => 'shell',
-	command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+  provider => 'shell',
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
